df18da4310bc75f9450e4cee2403f74df61fc0fa
Viviana9110 <v.londono.naranjo@gmail.com>
2025-05-20 21:41:20 -0500

    ultimo cambio

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://green-cart-git-main-viviana9110s-projects.vercel.app/']
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-eight-sigma.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
